b41c1738014bbda50ef170cbf3775ca043ea2e66
777bumer <777bumer.de@rambler.ru>
2022-05-15 18:14:48 +0300

    Initial commit New work .button,layout,

---
+++ app/src/main/java/com/example/lapapetun/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lapapetun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    //setTitle(R.string.add_item_titel);
+    }
+}
